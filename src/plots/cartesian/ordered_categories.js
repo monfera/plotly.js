9c366ce959b604ca55c09ddf49b357e712fcf0eb
Robert Monfera <monfera.robert@gmail.com>
2016-03-31 14:43:28 +0200

    lint orderedCategories

--- src/plots/cartesian/ordered_categories.js
+++ src/plots/cartesian/ordered_categories.js
@@ -16,18 +16,18 @@ var d3 = require('d3');
  * TODO add documentation
  */
 module.exports = function orderedCategories(axisLetter, categorymode, categorylist, data) {
-    
+
     return categorymode === 'array' ?
-        
+
         // just return a copy of the specified array ...
         categorylist.slice() :
-        
+
         // ... or take the union of all encountered tick keys and sort them as specified
         // (could be simplified with lodash-fp or ramda)
-        [].concat.apply([], data.map(function(d) {return d[axisLetter]}))
+        [].concat.apply([], data.map(function(d) {return d[axisLetter];}))
             .filter(function(element, index, array) {return index === array.indexOf(element);})
             .sort(({
-                'category ascending':  d3.ascending,
+                'category ascending': d3.ascending,
                 'category descending': d3.descending
             })[categorymode]);
 };
