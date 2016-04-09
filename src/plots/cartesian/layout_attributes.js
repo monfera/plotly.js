82076f2bca7ad7a1315fa79b6593fdc522afff9c
Robert Monfera <monfera.robert@gmail.com>
2016-04-09 08:40:24 +0200

    #189 commented out categorymode value ascending/descending as it's not part of this CR

--- src/plots/cartesian/layout_attributes.js
+++ src/plots/cartesian/layout_attributes.js
@@ -450,7 +450,7 @@ module.exports = {
         valType: 'enumerated',
         values: [
             'trace', 'category ascending', 'category descending',
-            'value ascending', 'value descending','array'
+            /*'value ascending', 'value descending',*/ 'array' // value ascending / descending to be implemented later
         ],
         dflt: 'trace',
         role: 'info',
@@ -459,8 +459,8 @@ module.exports = {
             'By default, plotly uses *trace*, which specifies the order that is present in the data supplied.',
             'Set `categorymode` to *category ascending* or *category descending* if order should be determined by',
             'the alphanumerical order of the category names.',
-            'Set `categorymode` to *value ascending* or *value descending* if order should be determined by the',
-            'numerical order of the values.',
+            /*'Set `categorymode` to *value ascending* or *value descending* if order should be determined by the',
+            'numerical order of the values.',*/ // // value ascending / descending to be implemented later
             'Set `categorymode` to *array* to derive the ordering from the attribute `categorylist`. If a category',
             'is not found in the `categorylist` array, the sorting behavior for that attribute will be identical to',
             'the *trace* mode. The unspecified categories will follow the categories in `categorylist`.'
