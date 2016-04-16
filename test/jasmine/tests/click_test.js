a3815f85731d9fff1a7ac6225a8420b5159999a4
Robert Monfera <monfera.robert@gmail.com>
2016-04-16 15:43:18 +0200

    Revert "#313 add currently failing test to check the preservation of the hover info even if hovermode is set to false"
    This reverts commit a04592765cf524f69574d476aff5acb461addb74.

--- test/jasmine/tests/click_test.js
+++ test/jasmine/tests/click_test.js
@@ -102,38 +102,6 @@ describe('click interactions', function() {
         });
     });
 
-    describe('click events with hovermode set to false', function() {
-        var futureData;
-
-        beforeEach(function(done) {
-            gd = createGraphDiv();
-
-            var mockCopy = Lib.extendDeep({}, mock);
-            Plotly.plot(gd, mockCopy.data, Lib.extendDeep({}, mockCopy.layout, {hovermode: false}))
-                .then(done);
-
-            gd.on('plotly_click', function(data) {
-                futureData = data;
-            });
-        });
-
-        it('should contain the correct fields despite hovermode: false', function() {
-            click(pointPos[0], pointPos[1]);
-            expect(futureData.points.length).toEqual(1);
-
-            var pt = futureData.points[0];
-            expect(Object.keys(pt)).toEqual([
-                'data', 'fullData', 'curveNumber', 'pointNumber',
-                'x', 'y', 'xaxis', 'yaxis'
-            ]);
-            expect(pt.curveNumber).toEqual(0);
-            expect(pt.pointNumber).toEqual(11);
-            expect(pt.x).toEqual(0.125);
-            expect(pt.y).toEqual(2.125);
-
-        });
-    });
-
     describe('double click events', function() {
         var futureData;
 
