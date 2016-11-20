d6e47bf8f138a8396814a567de415e4eabf2c05f
Robert Monfera <monfera.robert@gmail.com>
2016-11-20 21:54:14 +0100

    abstracting out parcoords

---
+++ src/traces/parcoords/parcoords.js
@@ -0,0 +1,40 @@
+/**
+* Copyright 2012-2016, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+'use strict';
+
+var d3 = require('d3');
+
+var config = require('./config');
+var model = require('./model');
+var overlay = require('./overlay');
+var vertexShaderSource = require('./shaderVertex');
+var fragmentShaderSource = require('./shaderFragment');
+var utils = require('./utils');
+var regl = require('regl');
+var unitToColor = require('./colors');
+var lineLayerMaker = require('./lineLayer');
+
+module.exports = function plot(root) {
+
+    var canvasGL = document.createElement('canvas');
+    canvasGL.setAttribute('style', 'position: absolute; margin: 32px;overflow: visible;');
+    root.appendChild(canvasGL);
+
+    var svgRoot = d3.select(root).append('svg')
+        .style('position', 'absolute')
+        .style('margin', '32px')
+        .style('overflow', 'visible')
+        .node();
+
+    var ol = overlay(svgRoot)
+
+    var lineLayer = lineLayerMaker(regl, utils, canvasGL, vertexShaderSource, fragmentShaderSource, config, model, ol, unitToColor);
+
+    lineLayer.render(false);
+};
