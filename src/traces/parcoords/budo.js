eecc198c5f17648b066ed54f2a36d345b17732fa
Robert Monfera <monfera.robert@gmail.com>
2016-11-20 23:05:46 +0100

    adding a budo entry point

---
+++ src/traces/parcoords/budo.js
@@ -0,0 +1,9 @@
+// for WIP development stage only, use it by
+// budo index.js --live -- -e budo.js -o bundle.js
+
+var parcoords = require('./parcoords');
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+
+parcoords(div);
