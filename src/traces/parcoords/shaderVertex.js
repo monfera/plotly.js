1355ac7956ee58caf7d1854e6c5620c461af82ae
Robert Monfera <monfera.robert@gmail.com>
2016-12-02 11:42:10 +0100

    increasing variable count

--- src/traces/parcoords/shaderVertex.js
+++ src/traces/parcoords/shaderVertex.js
@@ -3,13 +3,11 @@ module.exports = `
 precision lowp float;
 
 attribute vec4 p0, p1, p2, p3,
-    p4, p5, p6, p7,
-    p8, p9, pa, pb;
+               p4, p5, p6, p7,
+               p8, p9, pa, pb;
+               //pc, pd, pe, pf;
 
-attribute float colorIndex;
-
-attribute float x,
-    depth;
+attribute float x;
 
 uniform mat4 var1A, var2A, var1B, var2B, var1C, var2C,
     loA, hiA, loB, hiB, loC, hiC;
@@ -48,7 +46,7 @@ void main() {
     mat4 pA = mat4(p0, p1, p2, p3);
     mat4 pB = mat4(p4, p5, p6, p7);
     mat4 pC = mat4(p8, p9, pa, pb);
-
+    
     float show = float(mshow(pA, loA, hiA) &&
         mshow(pB, loB, hiB) &&
         mshow(pC, loC, hiC));
@@ -60,6 +58,8 @@ void main() {
 
     vec2 viewBoxXY = viewBoxPosition + viewBoxSize * vec2(x, y);
 
+    float depth = p0[0];
+    float colorIndex = 1.0 - p0[0];
     float depthOrHide = depth + 2.0 * (1.0 - show);
 
     gl_Position = vec4(
@@ -68,7 +68,6 @@ void main() {
         1.0
     );
 
-    //fragColor = vec4(colorIndex, 0, 1, 1);
     float clampedColorIndex = clamp((colorIndex - colorClamp[0]) / (colorClamp[1] - colorClamp[0]), 0.0, 1.0);
     fragColor = texture2D(palette, vec2((clampedColorIndex * 255.0 + 0.5) / 256.0, 0.5));
 }
