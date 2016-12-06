6d2f73334fadfb597c09bc8f4c47df23ed2735b5
Robert Monfera <monfera.robert@gmail.com>
2016-12-06 13:22:58 +0100

    removed control/Config dichotomy

--- src/traces/parcoords/config.js
+++ src/traces/parcoords/config.js
@@ -1,5 +1,7 @@
 module.exports = (function() {
 
+    var debug = false
+
     var width = 1480
     var height = 275
 
@@ -21,6 +23,32 @@ module.exports = (function() {
 
         // technical config
         canvasPixelRatio: 1, //window.devicePixelRatio, // using 2 or devicePixelRatio sharpens lines, slower
-        blockLineCount: 5000 // number of lines drawn in one increment
+        blockLineCount: 5000, // number of lines drawn in one increment
+
+
+
+
+        filterVisibleWidth: 4,
+        filterCaptureWidth: 48,
+        mousemoveThrottle: 1000 / 30,
+
+        handleGlyphHeight: 16,
+        handleGlyphOpacity: 0,
+        handleGlyphOverlap: 0, // effect can be seen with debug = true
+        filterBarFill: 'magenta',
+        filterBarFillOpacity: 1,
+        filterBarStroke: 'white',
+        filterBarStrokeOpacity: 1,
+        filterBarStrokeWidth: 1,
+        integerPadding: 0, // use 1 to offset integer extreme values by half pitch
+        averageTickDistance: 50, // in pixels,
+        verticalPadding: 2,
+
+        alphaBlending: false,
+
+        // for debugging:
+        captureZoneBorderColor: debug ? 'red' : "",
+        captureZoneFillColor: debug ? 'rgba(255, 0, 0, 0.05)' : 'rgba(0, 0, 0, 0)'
+
     }
 })();
