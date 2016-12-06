dd925131c892bada686af81f6a1a765e2c0ad07f
Robert Monfera <monfera.robert@gmail.com>
2016-12-06 13:53:20 +0100

    overlay => parcoords

--- src/traces/parcoords/lineLayer.js
+++ src/traces/parcoords/lineLayer.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2012-2016, Plotly, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
 var utils = require('./utils');
 var createREGL = require('regl');
 var vertexShaderSource = require('./shaderVertex');
