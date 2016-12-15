4559779fd2925dd9291ab8c7288169485d85543a
Robert Monfera <monfera.robert@gmail.com>
2016-12-15 13:30:06 +0100

    deleting pie vestigials 2

--- src/traces/parcoords/calc.js
+++ src/traces/parcoords/calc.js
@@ -9,132 +9,28 @@
 'use strict';
 
 var isNumeric = require('fast-isnumeric');
-var tinycolor = require('tinycolor2');
 var d3 = require('d3');
 
-function _convertArray(convert, data, count) {
-    var result = new Array(count),
-        data0 = data[0];
-
-    for(var i = 0; i < count; ++i) {
-        result[i] = (i >= data.length) ?
-            convert(data0) :
-            convert(data[i]);
-    }
-
-    return result;
-}
-
-var Color = require('../../components/color');
 var calcColorscale = require('./colorscale_calc');
-var helpers = require('./helpers');
 
 module.exports = function calc(gd, trace) {
     var vals = trace.dimensions,
-        labels = trace.labels,
         cd = [],
-        fullLayout = gd._fullLayout,
-        colorMap = fullLayout._piecolormap,
-        allThisTraceLabels = {},
-        needDefaults = false,
-        vTotal = 0,
-        hiddenLabels = fullLayout.hiddenlabels || [],
         i,
-        v,
-        label,
-        color,
-        hidden,
-        pt;
-
-    if(trace.dlabel) {
-        labels = new Array(vals.length);
-        for(i = 0; i < vals.length; i++) {
-            labels[i] = String(trace.label0 + i * trace.dlabel);
-        }
-    }
-
+        v;
     for(i = 0; i < vals.length; i++) {
-        v = vals.length - i // vals[i].pieChartCheat;
+        v = vals.length - i;
         if(!isNumeric(v)) continue;
         v = +v;
         if(v < 0) continue;
 
-        label = labels[i];
-        if(label === undefined || label === '') label = i;
-        label = String(label);
-        // only take the first occurrence of any given label.
-        // TODO: perhaps (optionally?) sum values for a repeated label?
-        if(allThisTraceLabels[label] === undefined) allThisTraceLabels[label] = true;
-        else continue;
-
-        color = tinycolor(trace.marker.colors[i]);
-        if(color.isValid()) {
-            color = Color.addOpacity(color, color.getAlpha());
-            if(!colorMap[label]) {
-                colorMap[label] = color;
-            }
-        }
-        // have we seen this label and assigned a color to it in a previous trace?
-        else if(colorMap[label]) color = colorMap[label];
-        // color needs a default - mark it false, come back after sorting
-        else {
-            color = false;
-            needDefaults = true;
-        }
-
-        hidden = hiddenLabels.indexOf(label) !== -1;
-
-        if(!hidden) vTotal += v;
-
         cd.push({
             v: v,
-            color: color,
             i: i,
-            hidden: hidden,
             integer: vals[i].integer,
             label: vals[i].label,
             values: vals[i].values
         });
-    }
-
-    //if(trace.sort) cd.sort(function(a, b) { return b.v - a.v; });
-
-    /**
-     * now go back and fill in colors we're still missing
-     * this is done after sorting, so we pick defaults
-     * in the order slices will be displayed
-     */
-
-    if(needDefaults) {
-        for(i = 0; i < cd.length; i++) {
-            pt = cd[i];
-            if(pt.color === false) {
-                colorMap[pt.label] = pt.color = nextDefaultColor(fullLayout._piedefaultcolorcount);
-                fullLayout._piedefaultcolorcount++;
-            }
-        }
-    }
-
-    // include the sum of all values in the first point
-    if(cd[0]) cd[0].vTotal = vTotal;
-
-    // now insert text
-    if(trace.textinfo && trace.textinfo !== 'none') {
-        var hasLabel = trace.textinfo.indexOf('label') !== -1,
-            hasText = trace.textinfo.indexOf('text') !== -1,
-            hasValue = trace.textinfo.indexOf('value') !== -1,
-            hasPercent = trace.textinfo.indexOf('percent') !== -1,
-            separators = fullLayout.separators,
-            thisText;
-
-        for(i = 0; i < cd.length; i++) {
-            pt = cd[i];
-            thisText = hasLabel ? [pt.label] : [];
-            if(hasText && trace.text[pt.i]) thisText.push(trace.text[pt.i]);
-            if(hasValue) thisText.push(helpers.formatPieValue(pt.v, separators));
-            if(hasPercent) thisText.push(helpers.formatPiePercent(pt.v / vTotal, separators));
-            pt.text = thisText.join('<br>');
-        }
     }
 
     // todo should it be in defaults.js?
@@ -178,29 +74,3 @@ module.exports = function calc(gd, trace) {
         filterbar: trace.filterbar
     }];
 };
-
-/**
- * pick a default color from the main default set, augmented by
- * itself lighter then darker before repeating
- */
-var pieDefaultColors;
-
-function nextDefaultColor(index) {
-    if(!pieDefaultColors) {
-        // generate this default set on demand (but then it gets saved in the module)
-        var mainDefaults = Color.defaults;
-        pieDefaultColors = mainDefaults.slice();
-
-        var i;
-
-        for(i = 0; i < mainDefaults.length; i++) {
-            pieDefaultColors.push(tinycolor(mainDefaults[i]).lighten(20).toHexString());
-        }
-
-        for(i = 0; i < Color.defaults.length; i++) {
-            pieDefaultColors.push(tinycolor(mainDefaults[i]).darken(20).toHexString());
-        }
-    }
-
-    return pieDefaultColors[index % pieDefaultColors.length];
-}
--- src/traces/parcoords/defaults.js
+++ src/traces/parcoords/defaults.js
@@ -26,11 +26,6 @@ var handleLineDefaults = function lineDefaults(traceIn, traceOut, defaultColor, 
         var lineColorDflt = (Array.isArray(markerColor) ? false : markerColor) || defaultColor;
         coerce('line.color', lineColorDflt);
     }
-/*
-    // parcoords doesn't have these but scatter does
-    coerce('line.width');
-    coerce('line.dash');
-*/
 };
 
 
@@ -70,8 +65,6 @@ module.exports = function supplyDefaults(traceIn, traceOut, defaultColor, layout
         return Lib.coerce(traceIn, traceOut, attributes, attr, dflt);
     }
 
-    var coerceFont = Lib.coerceFont;
-
     var dimensions = dimensionsDefaults(traceIn, traceOut);
 
     if(!Array.isArray(dimensions) || !dimensions.length) {
@@ -105,58 +98,4 @@ module.exports = function supplyDefaults(traceIn, traceOut, defaultColor, layout
     coerce('lines.focusalphablending');
     coerce('lines.verticalpadding');
     coerce('lines.integerpadding');
-
-
-    var labels = coerce('labels');
-    if(!Array.isArray(labels)) {
-        coerce('label0');
-        coerce('dlabel');
-    }
-
-    var lineWidth = coerce('marker.line.width');
-    if(lineWidth) coerce('marker.line.color');
-
-    var colors = coerce('marker.colors');
-    if(!Array.isArray(colors)) traceOut.marker.colors = []; // later this will get padded with default colors
-
-    coerce('scalegroup');
-    // TODO: tilt, depth, and hole all need to be coerced to the same values within a scaleegroup
-    // (ideally actually, depth would get set the same *after* scaling, ie the same absolute depth)
-    // and if colors aren't specified we should match these up - potentially even if separate pies
-    // are NOT in the same sharegroup
-
-
-    var textData = coerce('text');
-    var textInfo = coerce('textinfo', Array.isArray(textData) ? 'text+percent' : 'percent');
-
-    coerce('hoverinfo', (layout._dataLength === 1) ? 'label+text+value+percent' : undefined);
-
-    if(textInfo && textInfo !== 'none') {
-        var textPosition = coerce('textposition'),
-            hasBoth = Array.isArray(textPosition) || textPosition === 'auto',
-            hasInside = hasBoth || textPosition === 'inside',
-            hasOutside = hasBoth || textPosition === 'outside';
-
-        if(hasInside || hasOutside) {
-            var dfltFont = coerceFont(coerce, 'textfont', layout.font);
-            if(hasInside) coerceFont(coerce, 'insidetextfont', dfltFont);
-            if(hasOutside) coerceFont(coerce, 'outsidetextfont', dfltFont);
-        }
-    }
-
-    // 3D attributes commented out until I finish them in a later PR
-    // var tilt = coerce('tilt');
-    // if(tilt) {
-    //     coerce('tiltaxis');
-    //     coerce('depth');
-    //     coerce('shading');
-    // }
-
-    coerce('hole');
-
-    coerce('sort');
-    coerce('direction');
-    coerce('rotation');
-
-    coerce('pull');
 };
