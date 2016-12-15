618008bc944941b4d894f1f914fbd6589341c41f
Robert Monfera <monfera.robert@gmail.com>
2016-12-15 13:30:06 +0100

    deleting pie vestigials 2

--- src/traces/parcoords/attributes.js
+++ src/traces/parcoords/attributes.js
@@ -9,18 +9,13 @@
 'use strict';
 
 var colorAttributes = require('../../components/colorscale/color_attributes');
-var colorAttrs = require('../../components/color/attributes');
-var fontAttrs = require('../../plots/font_attributes');
-var plotAttrs = require('../../plots/attributes');
 var colorbarAttrs = require('../../components/colorbar/attributes');
 
 var extendFlat = require('../../lib/extend').extendFlat;
 
 module.exports = {
 
-    // PARCOORDS ATTRIBUTES
-
-    // todo I'm sure it could be DRIED up
+    // todo it could be DRYed up across other plots
     domain: {
         x: {
             valType: 'info_array',
@@ -31,7 +26,7 @@ module.exports = {
             ],
             dflt: [0, 1],
             description: [
-                'Sets the horizontal domain of this pie trace',
+                'Sets the horizontal domain of this `parcoords` trace',
                 '(in plot fraction).'
             ].join(' ')
         },
@@ -44,7 +39,7 @@ module.exports = {
             ],
             dflt: [0, 1],
             description: [
-                'Sets the vertical domain of this pie trace',
+                'Sets the vertical domain of this `parcoords` trace',
                 '(in plot fraction).'
             ].join(' ')
         }
@@ -264,183 +259,5 @@ module.exports = {
                 'A larger than zero value causes overlaps with the filter bar. The overlap is represented as pixels.'
             ]
         }
-    },
-
-
-
-
-    // OLD PIE CHART ATTRIBUTES BELOW
-
-
-    labels: {
-        valType: 'data_array',
-        description: 'Sets the dimension labels.'
-    },
-    // equivalent of x0 and dx, if label is missing
-    label0: {
-        valType: 'number',
-        role: 'info',
-        dflt: 0,
-        description: [
-            'Alternate to `labels`.',
-            'Builds a numeric set of labels.',
-            'Use with `dlabel`',
-            'where `label0` is the starting label and `dlabel` the step.'
-        ].join(' ')
-    },
-    dlabel: {
-        valType: 'number',
-        role: 'info',
-        dflt: 1,
-        description: 'Sets the label step. See `label0` for more info.'
-    },
-
-    marker: {
-        colors: {
-            valType: 'data_array',  // TODO 'color_array' ?
-            description: [
-                'Sets the color of each sector of this parallel coordinates chart.',
-                'If not specified, the default trace color set is used',
-                'to pick the sector colors.'
-            ].join(' ')
-        },
-
-        line: {
-            color: {
-                valType: 'color',
-                role: 'style',
-                dflt: colorAttrs.defaultLine,
-                arrayOk: true,
-                description: [
-                    'Sets the color of the line enclosing each sector.'
-                ].join(' ')
-            },
-            width: {
-                valType: 'number',
-                role: 'style',
-                min: 0,
-                dflt: 0,
-                arrayOk: true,
-                description: [
-                    'Sets the width (in px) of the line enclosing each sector.'
-                ].join(' ')
-            }
-        }
-    },
-
-    text: {
-        valType: 'data_array',
-        description: 'Sets text elements associated with each dimension.'
-    },
-
-    scalegroup: {
-        valType: 'string',
-        role: 'info',
-        dflt: '',
-        description: [
-            'If there are multiple pies that should be sized according to',
-            'their totals, link them by providing a non-empty group id here',
-            'shared by every trace in the same group.'
-        ].join(' ')
-    },
-
-    // labels (legend is handled by plots.attributes.showlegend and layout.hiddenlabels)
-    textinfo: {
-        valType: 'flaglist',
-        role: 'info',
-        flags: ['label', 'text', 'value', 'percent'],
-        extras: ['none'],
-        description: [
-            'Determines which trace information appear on the graph.'
-        ].join(' ')
-    },
-    hoverinfo: extendFlat({}, plotAttrs.hoverinfo, {
-        flags: ['label', 'text', 'value', 'percent', 'name']
-    }),
-    textposition: {
-        valType: 'enumerated',
-        role: 'info',
-        values: ['inside', 'outside', 'auto', 'none'],
-        dflt: 'auto',
-        arrayOk: true,
-        description: [
-            'Specifies the location of the `textinfo`.'
-        ].join(' ')
-    },
-    // TODO make those arrayOk?
-    textfont: extendFlat({}, fontAttrs, {
-        description: 'Sets the font used for `textinfo`.'
-    }),
-    insidetextfont: extendFlat({}, fontAttrs, {
-        description: 'Sets the font used for `textinfo` lying inside the pie.'
-    }),
-    outsidetextfont: extendFlat({}, fontAttrs, {
-        description: 'Sets the font used for `textinfo` lying outside the pie.'
-    }),
-
-    hole: {
-        valType: 'number',
-        role: 'style',
-        min: 0,
-        max: 1,
-        dflt: 0,
-        description: [
-            'Sets the fraction of the radius to cut out of the pie.',
-            'Use this to make a donut chart.'
-        ].join(' ')
-    },
-
-    // ordering and direction
-    sort: {
-        valType: 'boolean',
-        role: 'style',
-        dflt: true,
-        description: [
-            'Determines whether or not the samples are reordered',
-            'from largest to smallest.'
-        ].join(' ')
-    },
-    direction: {
-        /**
-         * there are two common conventions, both of which place the first
-         * (largest, if sorted) slice with its left edge at 12 o'clock but
-         * succeeding slices follow either cw or ccw from there.
-         *
-         * see http://visage.co/data-visualization-101-pie-charts/
-         */
-        valType: 'enumerated',
-        values: ['clockwise', 'counterclockwise'],
-        role: 'style',
-        dflt: 'counterclockwise',
-        description: [
-            'Specifies the direction at which succeeding sample lines follow',
-            'one another.'
-        ].join(' ')
-    },
-    rotation: {
-        valType: 'number',
-        role: 'style',
-        min: -360,
-        max: 360,
-        dflt: 0,
-        description: [
-            'Instead of the first slice starting at 12 o\'clock,',
-            'rotate to some other angle.'
-        ].join(' ')
-    },
-
-    pull: {
-        valType: 'number',
-        role: 'style',
-        min: 0,
-        max: 1,
-        dflt: 0,
-        arrayOk: true,
-        description: [
-            'Sets the fraction of larger radius to pull the sectors',
-            'out from the center. This can be a constant',
-            'to pull all slices apart from each other equally',
-            'or an array to highlight one or more slices.'
-        ].join(' ')
     }
 };
