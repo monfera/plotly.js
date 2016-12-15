2c8db8b7662b267e01e6fa5ef52cd02865068f7c
Robert Monfera <monfera.robert@gmail.com>
2016-12-15 16:19:10 +0100

    syntax + lint

--- src/traces/parcoords/devtesting/test.js
+++ src/traces/parcoords/devtesting/test.js
@@ -1,5 +1,12 @@
-// for WIP development stage only, use it by
-// budo index.js --live -- -e test.js -o bundle.js
+/**
+* Copyright 2012-2016, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+// todo remove the `devtesting` directory; for WIP development stage only
 
 var parcoords = require('./../parcoords');
 var mock = require('../../../../test/image/mocks/parcoords.json');
