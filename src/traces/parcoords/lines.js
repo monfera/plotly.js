monfera/plotly.js

commit 017ff01d850fc5d0dc7d694e8a348ddaac54600d

DRYing up inefficient placeholder code 1
(cherry picked from commit db0239b)

src/traces/parcoords/lines.js
@@ -296,21 +296,36 @@ module.exports = function(canvasGL, lines, canvasWidth, canvasHeight, data, unit
             }
         }
 
+        var dims = d3.range(2).map(function() {return d3.range(4).map(function() {return d3.range(16)});});
+
         // todo turn it into something DRYer and using efficient loops
         function makeItem(i, ii, x, panelSizeX, originalXIndex, scatter) {
+            var lr, mat, d, index;
+            var leftRight = [i, ii];
+
+            for(lr = 0; lr < 2; lr++) {
+                index = leftRight[lr]
+                for(mat = 0; mat < 4; mat++) {
+                    for (d = 0; d < 16; d++) {
+                        dims[lr][mat][d] = d + mat * 16 === index ? 1 : 0;
+                    }
+                }
+            }
+
             return {
                 key: originalXIndex,
                 resolution: [canvasWidth, canvasHeight],
                 viewBoxPosition: [x + overdrag, 0],
                 viewBoxSize: [panelSizeX, canvasPanelSizeY],
-                dim1A: d3.range(16).map(function(d) {return d === i ? 1 : 0;}),
-                dim2A: d3.range(16).map(function(d) {return d === ii ? 1 : 0;}),
-                dim1B: d3.range(16).map(function(d) {return d + 16 === i ? 1 : 0;}),
-                dim2B: d3.range(16).map(function(d) {return d + 16 === ii ? 1 : 0;}),
-                dim1C: d3.range(16).map(function(d) {return d + 32 === i ? 1 : 0;}),
-                dim2C: d3.range(16).map(function(d) {return d + 32 === ii ? 1 : 0;}),
-                dim1D: d3.range(16).map(function(d) {return d + 48 === i ? 1 : 0;}),
-                dim2D: d3.range(16).map(function(d) {return d + 48 === ii ? 1 : 0;}),
+                dim1A: dims[0][0],
+                dim1B: dims[0][1],
+                dim1C: dims[0][2],
+                dim1D: dims[0][3],
+                dim2A: dims[1][0],
+                dim2B: dims[1][1],
+                dim2C: dims[1][2],
+                dim2D: dims[1][3],
+
                 loA: d3.range(16).map(function(i) {return paddedUnit((!context && valid(i, 0) ? orig(i).filter[0] : 0)) - filterEpsilon;}),
                 hiA: d3.range(16).map(function(i) {return paddedUnit((!context && valid(i, 0) ? orig(i).filter[1] : 1)) + filterEpsilon;}),
                 loB: d3.range(16).map(function(i) {return paddedUnit((!context && valid(i, 16) ? orig(i + 16).filter[0] : 0)) - filterEpsilon;}),

commit a16aacbd556d4f3efe11f1f4c67eaa00222fdd3e

fixing incremental rendering regression introduced with 017ff01d DRYing up inefficient placeholder code

src/traces/parcoords/lines.js
@@ -268,9 +268,6 @@ module.exports = function(canvasGL, lines, canvasWidth, canvasHeight, dimensions
 
     var previousAxisOrder = [];
 
-    var dims = [0, 1].map(function() {return [0, 1, 2, 3].map(function() {return new Float32Array(16);});});
-    var lims = [0, 1].map(function() {return [0, 1, 2, 3].map(function() {return new Float32Array(16);});});
-
     function renderGLParcoords(dimensionViews, setChanged, clearOnly) {
 
         var I;
@@ -299,6 +296,9 @@ module.exports = function(canvasGL, lines, canvasWidth, canvasHeight, dimensions
         function makeItem(i, ii, x, panelSizeX, originalXIndex, scatter) {
             var loHi, abcd, d, index;
             var leftRight = [i, ii];
+
+            var dims = [0, 1].map(function() {return [0, 1, 2, 3].map(function() {return new Float32Array(16);});});
+            var lims = [0, 1].map(function() {return [0, 1, 2, 3].map(function() {return new Float32Array(16);});});
 
             for(loHi = 0; loHi < 2; loHi++) {
                 index = leftRight[loHi];

test/jasmine/tests/parcoords_test.js
@@ -31,7 +31,7 @@ var mock = {
             'contextcolor': [0, 0, 0],
             'contextopacity': 16,
             'pixelratio': 1,
-            'blocklinecount': 5000,
+            'blocklinecount': 50,
             'focusalphablending': false,
             'verticalpadding': 2,
             'integerpadding': 0
@@ -118,7 +118,7 @@ var mock = {
     }]
 };
 
-describe('parcoords', function() {
+fdescribe('parcoords', function() {
     var mockCopy = Lib.extendDeep({}, mock),
         gd;
 
@@ -126,7 +126,7 @@ describe('parcoords', function() {
         gd = createGraphDiv();
     });
 
-    afterEach(destroyGraphDiv);
+    //afterEach(destroyGraphDiv);
 
     it('should generate a parcoords plot', function(done) {
         Plotly.plot(gd, mockCopy.data, mockCopy.layout).then(done);