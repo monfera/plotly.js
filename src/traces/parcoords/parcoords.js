b3f61ec4756b900e28ea3b77d60f34425c987c02
Robert Monfera <monfera.robert@gmail.com>
2017-02-08 09:46:15 +0100

    switch from monospaced to default font

--- src/traces/parcoords/parcoords.js
+++ src/traces/parcoords/parcoords.js
@@ -225,7 +225,6 @@ function styleExtentTexts(selection) {
     selection
         .classed('axisExtentText', true)
         .attr('text-anchor', 'middle')
-        .style('font-family', 'monospace')
         .style('font-weight', 100)
         .style('font-size', 'x-small')
         .style('cursor', 'default')
@@ -560,7 +559,6 @@ module.exports = function(root, svg, styledData, layout, callbacks) {
 
     axisEnter
         .selectAll('text')
-        .style('font-family', 'monospace')
         .style('font-weight', 100)
         .style('font-size', 'x-small')
         .style('fill', 'black')
