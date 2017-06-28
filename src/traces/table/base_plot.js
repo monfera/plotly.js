387b9809888b77d118e3c8aa9433bee5840001f0
Robert Monfera <monfera.robert@gmail.com>
2017-06-29 01:57:02 +0200

    simplifying printing

--- src/traces/table/base_plot.js
+++ src/traces/table/base_plot.js
@@ -28,47 +28,8 @@ exports.clean = function(newFullData, newFullLayout, oldFullData, oldFullLayout)
     var hasTable = (newFullLayout._has && newFullLayout._has('table'));
 
     if(hadTable && !hasTable) {
-        oldFullLayout._paperdiv.selectAll('.table-line-layers').remove();
-        oldFullLayout._paperdiv.selectAll('.table-line-layers').remove();
         oldFullLayout._paperdiv.selectAll('.table').remove();
         oldFullLayout._paperdiv.selectAll('.table').remove();
         oldFullLayout._glimages.selectAll('*').remove();
     }
-};
-
-exports.toSVG = function(gd) {
-
-    var imageRoot = gd._fullLayout._glimages;
-    var root = d3.selectAll('.svg-container');
-    var canvases = root.filter(function(d, i) {return i === root.size() - 1;})
-        .selectAll('.table-lines.context, .table-lines.focus');
-
-    function canvasToImage(d) {
-        var canvas = this;
-        var imageData = canvas.toDataURL('image/png');
-        var image = imageRoot.append('svg:image');
-        var size = gd._fullLayout._size;
-        var domain = gd._fullData[d.model.key].domain;
-
-        image.attr({
-            xmlns: xmlnsNamespaces.svg,
-            'xlink:href': imageData,
-            x: size.l + size.w * domain.x[0] - c.overdrag,
-            y: size.t + size.h * (1 - domain.y[1]),
-            width: (domain.x[1] - domain.x[0]) * size.w + 2 * c.overdrag,
-            height: (domain.y[1] - domain.y[0]) * size.h,
-            preserveAspectRatio: 'none'
-        });
-    }
-
-    canvases.each(canvasToImage);
-
-    // Chrome / Safari bug workaround - browser apparently loses connection to the defined pattern
-    // Without the workaround, these browsers 'lose' the filter brush styling (color etc.) after a snapshot
-    // on a subsequent interaction.
-    // Firefox works fine without this workaround
-    window.setTimeout(function() {
-        d3.selectAll('#filterBarPattern')
-            .attr('id', 'filterBarPattern');
-    }, 60);
 };
